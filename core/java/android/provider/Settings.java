0cf8a61d69cb4255d7dc91a61ee28c2e032e3f1c
Roman Birg <roman@cyngn.com>
2017-10-28 19:05:10 -0400

    Add Settings.System toggle for quick-quick settings pulldown
    Change-Id: Id34a8faef279820148cf9c91c3f8a6cc320aa293
    Signed-off-by: AKabhishek <abhishekkaushik.star@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -3968,6 +3968,12 @@ public final class Settings {
         /** @hide */
         private static final Validator SHOW_BATTERY_PERCENT_VALIDATOR = sBooleanValidator;
 
+        /** Whether to allow one finger quick settings expansion on the right side of the statusbar.
+         *
+         * @hide
+         */
+        public static final String STATUS_BAR_QUICK_QS_PULLDOWN = "status_bar_quick_qs_pulldown";
+
         /**
          * Beginning of Pure System Settings Additions
          * @hide
